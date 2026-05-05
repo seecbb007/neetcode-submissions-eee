318ad7b4ace3299337d19f9056d50223f9819faf
seecbb007 <109918777+seecbb007@users.noreply.github.com>
2026-05-05 13:51:57 -0700

    Add: is-subsequence - submission-3

---
+++ Data Structures & Algorithms/is-subsequence/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+        i = 0
+        for j in range(len(t)):
+            if s[i] == t[j]:
+                i += 1
+                if len(s) == i:
+                    return True
+
+        return False
