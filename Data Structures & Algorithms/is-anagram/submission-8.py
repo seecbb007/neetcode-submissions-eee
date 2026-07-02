864ff869f8661e1e2ff81cd9d65bba798bc80c8c
seecbb007 <109918777+seecbb007@users.noreply.github.com>
2026-07-01 17:15:53 -0700

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        counts = Counter(s)
+        countt = Counter(t)
+        return counts == countt
