110e800ac6607e5742b2d79f23fd6719364c1502
seecbb007 <109918777+seecbb007@users.noreply.github.com>
2026-09-19 21:54:29 -0700

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -0,0 +1,57 @@
+class DoubleLList:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.head = DoubleLList(0,0)
+        self.tail = DoubleLList(0,0)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+    def add(self,node):
+        temp = self.head.next
+
+        node.prev = self.head
+        self.head.next = node
+
+        node.next = temp
+        temp.prev = node
+        
+    def remove(self,node):
+        prevNode = node.prev
+        nextNode = node.next
+
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+        self.remove(node)
+        self.add(node)
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        newnode = DoubleLList(key,value)
+
+        self.cache[key] = newnode
+        self.add(newnode)
+
+        if len(self.cache) > self.capacity:
+            lrunode = self.tail.prev
+            self.remove(lrunode)
+            del self.cache[lrunode.key]
